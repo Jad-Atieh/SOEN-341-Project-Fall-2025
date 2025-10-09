af719a082bde9d4e0f5d2010669f1e9e984d811a
denise-com <balabadenise@gmail.com>
2025-10-09 03:50:07 -0400

    made model Note

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from .models import Note
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,3 +14,8 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data) # Create a new user instance usiign the validated data
         return user
 
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
