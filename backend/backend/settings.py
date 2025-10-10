afae7885ee11b8c7e8dcfc3b7b2cf367d4fb7735
denise-com <balabadenise@gmail.com>
2025-10-09 21:58:33 -0400

    added User model using AbstractBaseUser

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     "api",
+    "api",
     "rest_framework",
     "corsheaders",
 ]
@@ -109,6 +109,8 @@ DATABASES = {
         }
     }
 }
+
+AUTH_USER_MODEL = 'api.User'
 
 
 # Password validation
