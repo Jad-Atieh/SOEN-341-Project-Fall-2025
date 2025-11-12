f7d5d7f503d98e44dfc95f8532d17af225cb8815
Tala Karraz <talakarraz@Talas-MacBook-Air.local>
2025-11-12 18:59:36 -0500

    Backend setup: DB config and initial api migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,3 +175,11 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+import os
+if os.getenv("SQLITE_DEV") == "1":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
